dc4357254ba97bc64b6eba1b1ebdf2d2e06a9d33
Naman Sharma <namanks0511@gmail.com>
2024-10-08 18:10:52 +0530

    initial commit

---
+++ app/src/main/java/com/example/edittexts/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.edittexts
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var name: EditText
+    lateinit var ok: Button
+    lateinit var result: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        name = findViewById(R.id.editTextName)
+        ok = findViewById(R.id.buttonOk)
+        result = findViewById(R.id.textViewResult)
+
+        ok.setOnClickListener {
+            var username: String = name.text.toString()
+            result.setText("Your name is " + username)
+        }
+    }
+}
