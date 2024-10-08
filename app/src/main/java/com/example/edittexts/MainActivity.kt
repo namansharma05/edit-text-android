c6b94e4e12a9a138f6707e538add6a1a5aa79e00
Naman Sharma <namanks0511@gmail.com>
2024-10-08 22:26:57 +0530

    used checkbox

--- app/src/main/java/com/example/edittexts/MainActivity.kt
+++ app/src/main/java/com/example/edittexts/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.edittexts
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -14,6 +15,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var name: EditText
     lateinit var ok: Button
     lateinit var result: TextView
+    lateinit var genderText: TextView
+    lateinit var male: CheckBox
+    lateinit var female: CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,10 +32,31 @@ class MainActivity : AppCompatActivity() {
         name = findViewById(R.id.editTextName)
         ok = findViewById(R.id.buttonOk)
         result = findViewById(R.id.textViewResult)
+        genderText = findViewById(R.id.textViewGender)
+        male = findViewById(R.id.checkBoxMale)
+        female = findViewById(R.id.checkBoxFemale)
 
         ok.setOnClickListener {
             var username: String = name.text.toString()
             result.setText("Your name is " + username)
+        }
+
+        male.setOnClickListener {
+            if(male.isChecked) {
+                genderText.text = "You are Male"
+                female.isChecked = false
+            } else {
+                genderText.text = "What is your Gender?"
+            }
+        }
+
+        female.setOnClickListener {
+            if(female.isChecked) {
+                genderText.text = "You are Female"
+                male.isChecked = false
+            } else {
+                genderText.text = "What is your Gender?"
+            }
         }
     }
 }
